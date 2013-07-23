45b4c5b97345128ecdb2ff4129075028e63fdf84
veganist <veganist@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-23 11:18:09 +0000

    use php longtags as shorttags might be buggy in some windows servers
    git-svn-id: https://plugins.svn.wordpress.org/last-seen-posts-widget/tags/1.1@744870 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lastseenposts.php
+++ lastseenposts.php
@@ -24,7 +24,7 @@ License: GPLv2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 ?>
-<?
+<?php
 /*
  * Session handling functions from http://wordpress.org/extend/plugins/simple-session-support/ by P.K. Wooster
  */
